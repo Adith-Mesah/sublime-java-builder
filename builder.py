aece356af3c05516f6c41c1cab045b7011365e77
adit mesah <73018048+syrx-adith@users.noreply.github.com>
2021-01-30 05:05:41 -0300

    Update builder.py
    fixed for non-package java program

--- builder.py
+++ builder.py
@@ -13,19 +13,27 @@ def asPath(package):
 
 def compile(file_name,package):
 	compilation = False
-	print("[INFO]: package \t:{}".format(package))
-	print("=======================================")
-	for file in os.listdir(asPath(package)):
-		if file == "{}.class".format(file_name):
-			d = os.system("del {}\\{}.class".format(asPath(package),file_name))
-			if d == 0:
-				print("[INFO]: deleted \t:{}.class".format(file_name))
-
+	if package:
+		print("[INFO]: package \t:{}".format(package))
+		print("=======================================")
+		for file in os.listdir(asPath(package)):
+			if file == "{}.class".format(file_name):
+				d = os.system("del {}\\{}.class".format(asPath(package),file_name))
+				if d == 0:
+					print("[INFO]: deleted \t:{}.class".format(file_name))
+	else:
+		d = os.system("del {}.class".format(file_name))
+		if d == 0:
+			print("[INFO]: deleted \t:{}.class".format(file_name))
 	os.system("javac -d . {}.java".format(file_name))
-
-	for file in os.listdir(asPath(package)):
-		if file == "{}.class".format(file_name):
-			compilation = True
+	if package:
+		for file in os.listdir(asPath(package)):
+			if file == "{}.class".format(file_name):
+				compilation = True
+	else:
+		for file in os.listdir("."):
+			if file == "{}.class".format(file_name):
+				compilation = True
 	return compilation
 
 
@@ -48,7 +56,7 @@ def remove_unc(array):
 def main():
 	print("=======================================")
 	print("[INFO]: file name \t:{}.java".format(argv[1]))
-	file_packge = None
+	file_package = None
 	cur_line = ""
 	with open(argv[1]+".java",'r') as unit:
 		while True: 
@@ -56,10 +64,10 @@ def main():
 			if len(cur_line) == 0:
 				break
 			if cur_line.__contains__("package"):
-				package = remove_unc(cur_line.split(" "))
+				file_package = remove_unc(cur_line.split(" "))
 				break;
-			print(cur_line)
-	compstat = compile(argv[1],package)
+			# print(cur_line)
+	compstat = compile(argv[1],file_package)
 	print("=======================================")
 	if compstat:
 		print("[INFO]: status \t\t:succeed")
@@ -69,8 +77,11 @@ def main():
 				print("=======================================")
 				if len(argv) == 4:
 					if argv[3] == "autoclear":
-						os.system("cls") 
-				os.system("java {}.{}".format(package,argv[1]))
+						os.system("cls")
+				if file_package:
+					os.system("java {}.{}".format(file_package,argv[1]))
+				else:
+					os.system("java {}".format(argv[1]))
 
 	else:
 		print("[INFO]: status \t\t:failed")
