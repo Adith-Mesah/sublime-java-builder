522c78ff6f993c046ed1cc867ccc6c861c48d52f
adith mesah <73018048+Adith-Mesah@users.noreply.github.com>
2021-02-04 01:10:35 -0300

    Update builder.py

--- builder.py
+++ builder.py
@@ -16,24 +16,34 @@ def compile(file_name,package):
 	# used to determine wether the compilation is a success or not
 	# which will be proccessed to decide if the application is runnable or not
 	compilation = False
+	comwdel = False
 	if package:
 		print("[INFO]: package \t:{}".format(package))
+		
 		print("=======================================")
 		# before we compile the file we check if the compiled-version of the java program is 
 		# already exist or not if it is then we will delete this file, by doing this we can
 		# check if the compilation is success by check if the compiled-version of the java file
 		# exist after compilation or not, if there is then we can determine that the compilation
 		# is a success
-		for file in os.listdir(asPath(package)):
-			if file == "{}.class".format(file_name):
-				d = os.system("del {}\\{}.class".format(asPath(package),file_name))
-				if d == 0:
-					print("[INFO]: deleted \t:{}.class".format(file_name))
+		try:
+			for file in os.listdir(asPath(package)):
+				if file == "{}.class".format(file_name):
+					d = os.system("del {}\\{}.class".format(asPath(package),file_name))
+					if d == 0:
+						print("[INFO]: deleted \t:{}.class".format(file_name))
+		except FileNotFoundError:
+			print("[INFO]: package {} not found, performing compilation without deleting previous version".format(package))
+			print("[INFO]: compiling {}.java...".format(argv[1]))
+			os.system("javac -d . {}.java".format(file_name))
+			comwdel = True
 	else:
 		d = os.system("del {}.class".format(file_name))
 		if d == 0:
 			print("[INFO]: deleted \t:{}.class".format(file_name))
-	os.system("javac -d . {}.java".format(file_name))
+	if not comwdel:
+		print("[INFO]: compiling {}.java...".format(argv[1]))
+		os.system("javac -d . {}.java".format(file_name))
 
 	# check if the compiled-version of java program is exist or not
 	# if exist return True and False otherwise.
@@ -55,7 +65,7 @@ def remove_unc(array):
 	new_arr = []
 
 	def checkForNewLineAndSemiColon(string):
-		"""delete the new-line character and semi-color from the string"""
+		"""delete the new-line character and semi-colon from the string"""
 		new_string = ""
 		for i in string:
 			if i != "\n" and i != ";":
@@ -77,7 +87,7 @@ def main():
 	# parse the the java file as lines and look for the string 'package'
 	# then call the remove_unc to remove unnecessary characters in the line of where the package
 	# string found and if not found then the file_package will keep None which will be processed 
-	# by the compile function and will used by in the compilation and launching
+	# by the compile function and will used as compilation status
 	with open(argv[1]+".java",'r') as unit:
 		while True: 
 			cur_line = unit.readline()
@@ -85,7 +95,7 @@ def main():
 				break
 			if cur_line.__contains__("package"):
 				file_package = remove_unc(cur_line.split(" "))
-				break;
+				break
 	compstat = compile(argv[1],file_package)
 	print("=======================================")
 
